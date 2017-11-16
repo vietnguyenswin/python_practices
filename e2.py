ad84ae5993144b07f815e8cf97bf8f275e713bc9
vietnguyenswin <vietnguyenswin@outlook.com>
2017-11-16 14:47:53 +1100

    upload e2

---
+++ e2.py
@@ -0,0 +1,26 @@
+"""
+    Author: vietnguyenswin
+    Name: Exercise 2
+    Description: String processing with Python
+    Last modified: 16 Nov 2017
+"""
+
+
+def input_string():
+    return raw_input("Please submit the string: ")
+
+
+def string_process():
+    # Assigning value for a vairbl
+    s = input_string()
+    if s.isalpha():
+        print "input is alpha characters"
+    elif s.isdigit():
+        print "input is digit characters"
+    # Converting from list to tuple
+    tuples = tuple(list(s))
+    print tuples
+
+
+while True:
+    string_process()
