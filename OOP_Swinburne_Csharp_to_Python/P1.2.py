04882be45b7e6a92b23191902a9a0f6900850777
vietnguyenswin <vietnguyenswin@outlook.com>
2017-11-18 00:27:56 +1100

    Task P1.2

---
+++ OOP_Swinburne_Csharp_to_Python/P1.2.py
@@ -0,0 +1,73 @@
+"""
+    Author: vietnguyenswin (Viet Nguyen)
+    Name: Pass task 1.2P
+    Description: + Object-Oriented Programming with Python programming language.
+                 + This is a rework version of required tasks for Object-Oriented Programming with C# unit
+                    (COS20007 - Swinburne Semester 2 2016).
+                 + The author understands the principles of Object-Oriented Analysis & Design and achieved Distinction
+                    grade (COS20007).
+                 + The motivation for doing this version is to practice using Python.
+                 + The works of this version are public. Nevertheless, you should not copy & paste without understanding
+                    the mechanisms behind.
+    Instruction: + Compile the code then run to see the result.
+    Last modified: 18 Nov 2017
+"""
+
+
+class Counter(object):
+    def __init__(self, name):
+        self.__name = name
+        self.__count = 0
+
+    def increment(self):
+        self.__count += 1
+
+    def reset(self):
+        self.__count = 0
+
+    # accessor (get) property
+    @property
+    def value(self):
+        return self.__count
+
+    @property
+    def name(self):
+        return self.__name
+
+    # mutator (set) property
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+
+class Main(Counter):
+    # static method. (notice that there is no "self")
+    @staticmethod
+    def printing(counters):
+        # similar to foreach loop
+        for c in counters:
+            # string format with Python
+            print("{0} is {1}".format(c.name, c.value))
+
+    @staticmethod
+    def act():
+        # create new instances of Counter object
+        my_counters1 = Counter("Counter 1")
+        my_counters2 = Counter("Counter 2")
+        my_counters3 = my_counters1
+        # Assign the instances to the instance list
+        my_counters = [my_counters1, my_counters2, my_counters3]
+
+        # for loop
+        for i in range(0, 4):
+            # call the method
+            my_counters1.increment()
+
+        for i in range(0, 9):
+            my_counters2.increment()
+
+        Main.printing(my_counters)
+        my_counters3.reset()
+        Main.printing(my_counters)
+
+Main.act()
