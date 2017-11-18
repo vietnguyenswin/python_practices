d58a9435b10abe06e2ede5dc2c7dd8a1cde2f2a4
vietnguyenswin <vietnguyenswin@outlook.com>
2017-11-18 23:39:29 +1100

    Upload P3.2

---
+++ OOP_Swinburne_Csharp_to_Python/P3.2.py
@@ -0,0 +1,74 @@
+"""
+    Author: vietnguyenswin (Viet Nguyen)
+    Name: Pass task 3.2P
+    Description: + Object-Oriented Programming with Python programming language.
+                 + This is a rework version of required tasks for Object-Oriented Programming with C# unit
+                    (COS20007 - Swinburne Semester 2 2016).
+                 + The author understands the principles of Object-Oriented Analysis & Design and achieved Distinction
+                    grade (COS20007).
+                 + The motivation for doing this version is to practice using Python.
+                 + The works of this version are public. Nevertheless, you should not copy & paste without understanding
+                    the mechanisms behind.
+    Instruction: + Use cd command to redirect the terminal working directory into the folder which you store this file.
+                 + Execute this command: python P3.2.py
+    Last modified: 18 Nov 2017
+"""
+
+import time
+import os
+
+# Define Counter class for Counter object
+class Counter(object):
+    # new instance of Counter object will be assigned init value = 0 when created
+    def __init__(self):
+        self.__count = 0
+
+    # public methods & they are parts of the class interface
+    def tick(self):
+        self.__count += 1
+
+    def reset(self):
+        self.__count = 0
+
+    # property: getter | clock object will send message to Counter object to use this property
+    @property
+    def value(self):
+        return self.__count
+
+# Define Clock class
+class Clock(Counter):
+    @staticmethod
+    def mechanism():
+        # Create new instances of Counter object
+        hour = Counter()
+        minute = Counter()
+        second = Counter()
+        # Infinity loop
+        while True:
+            # Tick the second just like the functional clock
+            second.tick()
+            # Hold on, wait for 1 second until another tick
+            time.sleep(1)
+            if second.value >= 60:
+                minute.tick()
+                second.reset()
+            if minute.value >= 60:
+                hour.tick()
+                minute.reset()
+            if hour.value >= 24:
+                hour.reset()
+
+            # check if the current OS system is Windows or Mac
+            # clear previous printed message before printing the new one
+            if os.name != "nt":
+                os.system("clear")
+            else:
+                os.system("cls")
+            # print the clock
+            # use format function | 0>2d: 5 --> 05
+            print("{0:0>2d}:{1:0>2d}:{2:0>2d}".format(hour.value, minute.value, second.value))
+
+# create an instance of the Clock object
+clock = Clock()
+# call the method
+clock.mechanism()
