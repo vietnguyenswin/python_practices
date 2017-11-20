22d7e6c5f54e1c3ae4bb7fe2c2889bccdf6b6def
vietnguyenswin <vietnguyenswin@outlook.com>
2017-11-21 10:45:36 +1100

    Swin Adventure init upload

---
+++ OOP_Swinburne_Csharp_to_Python/C2.2.py
@@ -0,0 +1,14 @@
+"""
+    Author: vietnguyenswin (Viet Nguyen)
+    Name: Pass task 2.2C
+    Description: + Object-Oriented Programming with Python programming language.
+                 + This is a rework version of required tasks for Object-Oriented Programming with C# unit
+                    (COS20007 - Swinburne Semester 2 2016).
+                 + The author understands the principles of Object-Oriented Analysis & Design and achieved Distinction
+                    grade (COS20007).
+                 + The motivation for doing this version is to practice using Python.
+                 + The works of this version are public. Nevertheless, you should not copy & paste without understanding
+                    the mechanisms behind.
+    Instruction: + Update later
+    Last modified: 21 Nov 2017
+"""
