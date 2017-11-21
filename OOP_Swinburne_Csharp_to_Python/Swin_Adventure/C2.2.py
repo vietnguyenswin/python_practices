7d5de891d5e9b58bd08288fa49230a7d2f554a43
vietnguyenswin <vietnguyenswin@outlook.com>
2017-11-21 22:28:58 +1100

    update C2.2

--- OOP_Swinburne_Csharp_to_Python/Swin_Adventure/C2.2.py
+++ OOP_Swinburne_Csharp_to_Python/Swin_Adventure/C2.2.py
@@ -13,19 +13,32 @@
     Last modified: 21 Nov 2017
 """
 
-
+# define Identifiable class
 class Identifiable(object):
+    # constructor adds identifiers to the Identifiable object from the passed list
     def __init__(self, idents):
         self.__identifiers = list(idents)
 
+    # check if the passed in identifier is in the __identifiers
     def are_you(self, id):
         if id in self.__identifiers:
-            # return True
-            print("oh yeah")
+            return True
+            # print("oh yeah")
         else:
-            # return False
-            print("oh no")
+            return False
+            # print("oh no")
+
+    # return the first identifier in __identifiers
+    @property
+    def first_id(self):
+        return self.__identifiers[0]
+
+    # convert the identifier to lowercase and add it to __identifiers list
+    def add_identifier(self, new_id):
+        self.__identifiers.append(str(new_id).lower())
 
 
 test = Identifiable(["hello", "hi"])
-test.are_you("hello")
+test.add_identifier("ajinomoto")
+test.are_you("ajinomoto")
+print test.first_id
