c0398b68cceee2ccd894efbe4e0b1897ecb33422
vietnguyenswin <vietnguyenswin@outlook.com>
2017-11-24 00:20:46 +1100

    Update Inventory

--- OOP_Swinburne_Csharp_to_Python/Swin_Adventure/C2.2.py
+++ OOP_Swinburne_Csharp_to_Python/Swin_Adventure/C2.2.py
@@ -14,6 +14,7 @@
 """
 import unittest
 
+
 # define Identifiable class
 class Identifiable(object):
     # constructor adds identifiers to the Identifiable object from the passed list
@@ -37,27 +38,6 @@ class Identifiable(object):
     # convert the identifier to lowercase and add it to __identifiers list
     def add_identifier(self, new_id):
         self.__identifiers.append(str(new_id).lower())
-
-
-class TestsIteration(unittest.TestCase):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def test_are_you(self):
-        test = Identifiable(["hello", "hi"])
-        self.assertEqual(True, test.are_you("hello"))
-
-    def test_first_id(self):
-        test = Identifiable(["hello", "hi"])
-        self.assertEqual("hello", test.first_id)
-
-    def test_add_identifier(self):
-        test = Identifiable([])
-        test.add_identifier("hello")
-        self.assertEqual(True, test.are_you("hello"))
 
 
 class GameObject(Identifiable):
@@ -86,11 +66,13 @@ class Item(GameObject):
             self.__name = name
             self.__desc = desc
 
+
 class Player(GameObject):
     def __init__(self, name, desc):
         super(Player, self).__init__(["me", "inventory"], name, desc)
         self.__name = name
         self.__desc = desc
+
 
 class Inventory(object):
     def __init__(self):
@@ -100,8 +82,7 @@ class Inventory(object):
         for itm in self.__items:
             if itm.are_you(input_id):
                 return True
-            else:
-                return False
+        return False
 
     def put_item(self, item):
         self.__items.append(item)
@@ -110,90 +91,98 @@ class Inventory(object):
         for itm in self.__items:
             if itm.are_you(input_id):
                 self.__items.remove(itm)
-                return itm
 
     def item_fetch(self, input_id):
         for itm in self.__items:
             if itm.are_you(input_id):
                 return itm
 
+    @property
+    def item_list(self):
+        list_details = ""
+        for itm in self.__items:
+            list_details += itm.short_description + "\n"
+        return list_details
+
+class TestsIteration(unittest.TestCase):
+    def setUp(self):
+        self.test = Identifiable(["hello", "hi"])
+
+    def tearDown(self):
+        pass
+
+    def test_are_you(self):
+        self.assertEqual(True, self.test.are_you("hello"))
+
+    def test_first_id(self):
+        self.assertEqual("hello", self.test.first_id)
+
+    def test_add_identifier(self):
+        self.assertEqual(True, self.test.are_you("hello"))
+
 class TestInventoryClass(unittest.TestCase):
     def setUp(self):
-        pass
+        self.inv = Inventory()
+        self.shovel = Item(["shovel", "spade"], "a shovel", "This is a might fine...")
+        self.computer = Item(["computer", "pc"], "a computer", "This is a desktop")
+        self.inv.put_item(self.shovel)
+        self.inv.put_item(self.computer)
 
     def tearDown(self):
         pass
 
     def test_has_item_and_put_item(self):
-        shovel = Item(["shovel", "spade"], "a shovel", "This is a might fine...")
-        inv = Inventory()
-        inv.put_item(shovel)
-        self.assertTrue(inv.has_item("spade"))
-        self.assertTrue(inv.has_item("shovel"))
-        self.assertFalse(inv.has_item("abc"))
+        self.assertTrue(self.inv.has_item("spade"))
+        self.assertTrue(self.inv.has_item("shovel"))
+        self.assertTrue(self.inv.has_item("computer"))
+        self.assertTrue(self.inv.has_item("pc"))
+        self.assertFalse(self.inv.has_item("abc"))
 
     def test_item_fetch(self):
-        inv = Inventory()
+        self.assertIsNotNone(self.inv.item_fetch("shovel"))
+        self.assertIsNotNone(self.inv.item_fetch("spade"))
+        self.assertIsNotNone(self.inv.item_fetch("computer"))
+        self.assertIsNotNone(self.inv.item_fetch("pc"))
+        self.assertIsNone(self.inv.item_fetch("abc"))
 
-        shovel = Item(["shovel", "spade"], "a shovel", "This is a might fine...")
-        inv.put_item(shovel)
-        self.assertIsNotNone(inv.item_fetch("shovel"))
-        self.assertIsNotNone(inv.item_fetch("spade"))
+    def test_take_item(self):
+        self.assertIsNotNone(self.inv.item_fetch("shovel"))
+        self.assertIsNotNone(self.inv.item_fetch("computer"))
+        self.assertIsNone(self.inv.take_item("shovel"))
+        self.assertIsNone(self.inv.take_item("computer"))
 
-        computer = Item(["computer", "pc"], "a computer", "This is a desktop")
-        inv.put_item(computer)
-        self.assertIsNotNone(inv.item_fetch("computer"))
-        self.assertIsNotNone(inv.item_fetch("pc"))
-
-        self.assertIsNone(inv.item_fetch("abc"))
-
-    def take_item(self):
-        inv = Inventory()
-
-        shovel = Item(["shovel", "spade"], "a shovel", "This is a might fine...")
-        inv.put_item(shovel)
-        self.assertIsNotNone(inv.item_fetch("shovel"))
-        self.assertIsNone(inv.take_item("shovel"))
-
-        computer = Item(["computer", "pc"], "a computer", "This is a desktop")
-        inv.put_item(computer)
-        self.assertIsNotNone(inv.item_fetch("computer"))
-        self.assertIsNone(inv.take_item("computer"))
+    def test_item_list(self):
+        self.assertEqual("a shovel (shovel)\na computer (computer)\n", self.inv.item_list)
 
 class TestItemClass(unittest.TestCase):
     def setUp(self):
-        pass
+        self.shovel = Item(["shovel", "spade"], "a shovel", "This is a might fine...")
 
     def tearDown(self):
         pass
 
     def test_item_is_identifiable(self):
-        shovel = Item(["shovel", "spade"], "a shovel", "This is a might fine...")
-        self.assertTrue(shovel.are_you("shovel"))
+        self.assertTrue(self.shovel.are_you("shovel"))
 
     def test_item_name(self):
-        shovel = Item(["shovel", "spade"], "a shovel", "This is a might fine...")
-        self.assertEqual("shovel", shovel.name)
+        self.assertEqual("shovel", self.shovel.name)
 
     def test_item_short_description(self):
-        shovel = Item(["shovel", "spade"], "a shovel", "This is a might fine...")
-        self.assertEqual("a shovel (shovel)", shovel.short_description)
+        self.assertEqual("a shovel (shovel)", self.shovel.short_description)
 
     def test_item_long_description(self):
-        shovel = Item(["shovel", "spade"], "a shovel", "This is a might fine...")
-        self.assertEqual("This is a might fine...", shovel.long_description)
+        self.assertEqual("This is a might fine...", self.shovel.long_description)
 
 class TestPlayerClass(unittest.TestCase):
     def setUp(self):
-        pass
+        self.player = Player("Viet", "Python programmer")
 
     def tearDown(self):
         pass
 
     def test_player_is_identifiable(self):
-        player = Player("Viet", "Python programmer")
-        self.assertTrue(player.are_you("me"))
-        self.assertTrue(player.are_you("inventory"))
+        self.assertTrue(self.player.are_you("me"))
+        self.assertTrue(self.player.are_you("inventory"))
 
 
 if __name__ == '__main__':
